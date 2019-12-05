591cd6819f3b4b84cdd8600fe321d49084fc7fa3
Moiseev Roman <lp15.mr@yandex.ru>
2019-12-05 21:36:57 +0300

    Second commit

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from weather import weather_by_city
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Привет!'
+    weather = weather_by_city('Tver,Russia')
+
+    if weather:
+        tempr = weather["temp_C"]
+        feels = weather['FeelsLikeC']
+        return f"Погода: {tempr}, ощущается как {feels}"
+    else:
+        return "Сервис погоды временно не доступен"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def weather_by_city(city_name):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    params = {
+        'key': '290249d9abc34900bf8170408190512',
+        'q': city_name,
+        'format': 'json',
+        'num_of_days': 1,
+        'lang': "ru"
+    }
+    result = requests.get(weather_url, params=params)
+    weather = result.json()
+    if 'data' in weather:
+        if "current_condition" in weather['data']:
+            try:
+                return weather["data"]["current_condition"][0]
+            except(IndexError, TypeError):
+                return False
+    return False
+
+
+if __name__ == "__main__":
+    print(weather_by_city('Tver, Russia'))
