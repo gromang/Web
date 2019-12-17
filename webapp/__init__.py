d9c19cd93cdbc99ea36ea623c621e4d181855b7d
Roman Moiseev <lp15.mr@yandex.ru>
2019-12-17 16:09:47 +0300

    Refactoring

---
+++ webapp/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    @app.route('/')
+    def index():
+        title = "Новости Python"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news_list = get_python_news()
+        return render_template('index.html', title=title, weather=weather, news_list=news_list)
+
+    return app
+
+
---
+++ webapp/config.py
@@ -0,0 +1,3 @@
+WEATHER_DEFAULT_CITY = "Tver,Russia"
+WEATHER_API_KEY = "290249d9abc34900bf8170408190512"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
