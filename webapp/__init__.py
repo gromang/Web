2e253410d67c42cf5139bec99eb45e7327df5b31
Moiseev Roman <lp15.mr@yandex.ru>
2020-01-08 23:29:44 +0300

    sixth week part1

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 from webapp.weather import weather_by_city
 from webapp.model import db, News
+from webapp.forms import LoginForm
 
 
 def create_app():
@@ -17,8 +18,13 @@ def create_app():
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', title=title, weather=weather, news_list=news_list)
 
-    return app
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template("login.html", title=title, form=login_form)
 
+    return app
 
 # Linux и Mac:   export FLASK_APP=webapp && export FLASK_ENV=development && flask run
 # Windows:       set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
