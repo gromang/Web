227a23afb2779a335f5c18f34936fc23ee139861
Roman Moiseev <lp15.mr@yandex.ru>
2020-01-10 16:34:57 +0300

    8.4

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user
 
 from webapp.weather import weather_by_city
 from webapp.model import db, News, User
@@ -35,7 +35,21 @@ def create_app():
     @app.route('/process-login', methods=['POST'])
     def process_login():
         form = LoginForm()
-        if
+        if form.validate_on_submit():
+            user = User.query.filter_by(username = form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        flash("Вы успешно разлогинились")
+        return redirect(url_for("index"))
 
     return app
 
