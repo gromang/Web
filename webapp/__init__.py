1745ebad62939d07481010908a8caa830ff6631c
Moiseev Roman <lp15.mr@yandex.ru>
2020-01-10 22:57:40 +0300

    finish 6 week

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user, logout_user
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 from webapp.weather import weather_by_city
 from webapp.model import db, News, User
@@ -28,6 +28,8 @@ def create_app():
 
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = "Авторизация"
         login_form = LoginForm()
         return render_template("login.html", title=title, form=login_form)
@@ -36,7 +38,7 @@ def create_app():
     def process_login():
         form = LoginForm()
         if form.validate_on_submit():
-            user = User.query.filter_by(username = form.username.data).first()
+            user = User.query.filter_by(username=form.username.data).first()
             if user and user.check_password(form.password.data):
                 login_user(user)
                 flash('Вы вошли на сайт')
@@ -50,6 +52,14 @@ def create_app():
         logout_user()
         flash("Вы успешно разлогинились")
         return redirect(url_for("index"))
+
+    @app.route("/admin")
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return "Привет, админ"
+        else:
+            return "Ты не админ"
 
     return app
 
