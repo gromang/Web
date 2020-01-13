3572194440eb092ab911586c82b8862787d77c7e
Roman Moiseev <lp15.mr@yandex.ru>
2020-01-13 17:24:00 +0300

    migration step1

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from webapp.db import db
 from webapp.user.models import User
@@ -12,6 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
